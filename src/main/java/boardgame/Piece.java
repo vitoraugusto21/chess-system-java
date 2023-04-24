a906e2e89642ae50d08406336ccb4313e17d6475
Vítor Augusto <vitoraug210@gmail.com>
2023-04-24 15:34:34 -0300

    Update Piece.java

--- src/main/java/boardgame/Piece.java
+++ src/main/java/boardgame/Piece.java
@@ -1,4 +1,19 @@
 package boardgame;
 
 public class Piece {
+
+    /* posição de matriz, garantir que a posição ainda não seja acessada*/
+    protected Position position;
+    private Board board;
+
+    /*posição de uma peça recem criada, será inicialmente nula*/
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+
+    /*protected para que somente classes dentro do mesmo pacote ou subclasses acessem*/
+    protected Board getBoard() {
+        return board;
+    }
 }
